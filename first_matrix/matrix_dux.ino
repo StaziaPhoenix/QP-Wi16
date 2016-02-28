03be70a57d0e9e49151cd7c03d09af6fc52ac4af
dal116 <dlee13888@gmail.com>
2016-02-27 16:12:07 -0800

    Added files via upload

--- first_matrix/matrix_dux.ino
+++ first_matrix/matrix_dux.ino
@@ -1,6 +1,3 @@
-#include <DHT.h>
-#include <idDHT11.h>
-
 //Pin connected to latch pin (ST_CP) of 74HC595
 const int latchPin = 13;
 //Pin connected to clock pin (SH_CP) of 74HC595
@@ -8,45 +5,42 @@ const int clockPin = 11;
 ////Pin connected to Data in (DS) of 74HC595
 const int dataPin = 9;
 
+//columns of the led grid 
 byte col0 = 2;
 byte col1 = 3;
 byte col2 = 4;
 byte col3 = 5;
 byte col4 = 6;
 
+int numCol = 5; 
+int numRow = 5;
+
+//brightness setting 
 int bright = 0;
+//fade setting 
 int fade = 200;
 int shock = 7; 
 int val;
 
+//flag for the button key switch 
 int modeFlag = 0;
+
+//joystick x and y axis
 int joyX = A3;
 int joyY = A4; 
-
-int togglePwr = 8;
-int prevToggle; 
-
-//byte row0 = 22;
-//byte row1 = 25;
-//byte row2 = 26;
-
-byte colDataInit[5] = {0xFf, 0xFf, 0xff, 0xFf, 0xFf};
-byte colData[5] = {0, 0, 0, 0, 0};
-byte cols[5] = {col0, col1, col2, col3, col4};
-
-//temp sens
-//void dht11_wrapper(); 
-//idDHT11 DHT11(DHT11pin , DHT11int, dht11_wrapper); 
 
 void setup() {
   Serial.begin(9600);
   //Serial.begin(115200);
+
+  //led grid columns 
   pinMode(col0,OUTPUT);
   pinMode(col1,OUTPUT);
   pinMode(col2,OUTPUT);
   pinMode(col3,OUTPUT);
   pinMode(col4,OUTPUT);
 
+//initialize grid off 
   digitalWrite(col0,LOW);
   digitalWrite(col1,LOW);
   digitalWrite(col2,LOW);
@@ -56,13 +50,13 @@ void setup() {
   pinMode(latchPin,OUTPUT);
   pinMode(dataPin,OUTPUT);
   pinMode(clockPin,OUTPUT);
-  
-  pinMode(togglePwr, INPUT);
+
+  //vibration sensor 
   pinMode(shock, INPUT); 
 
+  //
   pinMode(joyX, INPUT);
   pinMode(joyY, INPUT);
-
   for (int i = 0; i<5; i++) {
     digitalWrite(cols[i],HIGH);
     
@@ -74,13 +68,6 @@ void setup() {
     digitalWrite(cols[i], LOW);
   }
 }
-/*
-void dht11_wrapper()
-{
-  DHT11.isrCallback();
-}
-*/
-
 void loop() { 
 
   // If conditions for checking sensor states
@@ -129,16 +116,6 @@ void loop() {
   }
  */
 
- //joyStk();
- /*
- Serial.print("joyX analog :");
-  Serial.print(analogRead(joyX));
-  Serial.print("\n");
-  Serial.print("joyY analog :");
-  Serial.print(analogRead(joyY));
-  Serial.print("\n");
-  delay(500); 
-  */
   /*
   setData();
 
@@ -154,8 +131,9 @@ void loop() {
   }
   */
   //temp();
-  //toggle();
 }
+
+//turns off the grid 
 void clearGrid() {
   for(int i = 0; i < 5; i++) 
   {
@@ -163,6 +141,7 @@ void clearGrid() {
   }
 }
 
+//reads input from vibration sensor
 void vibration() {
   Serial.print(digitalRead(shock));
   if (digitalRead(shock) == HIGH)
@@ -213,61 +192,9 @@ void adjustData(int i) {
 }
 void pattern1() 
 {
-  
-}
-/*
-void temp()
-{
-  DHT11.acquire();
-  while(DHT11.acquiring())
-  ;
-  int result = DHT11.getStatus(); 
-  switch (result)
+  for (int i = 0; i < numCol; i++)
   {
-  case IDDHTLIB_OK: 
-    Serial.println("OK"); 
-    break;
-  case IDDHTLIB_ERROR_CHECKSUM: 
-    Serial.println("Error\n\r\tChecksum error"); 
-    break;
-  case IDDHTLIB_ERROR_ISR_TIMEOUT: 
-    Serial.println("Error\n\r\tISR Time out error"); 
-    break;
-  case IDDHTLIB_ERROR_RESPONSE_TIMEOUT: 
-    Serial.println("Error\n\r\tResponse time out error"); 
-    break;
-  case IDDHTLIB_ERROR_DATA_TIMEOUT: 
-    Serial.println("Error\n\r\tData time out error"); 
-    break;
-  case IDDHTLIB_ERROR_ACQUIRING: 
-    Serial.println("Error\n\r\tAcquiring"); 
-    break;
-  case IDDHTLIB_ERROR_DELTA: 
-    Serial.println("Error\n\r\tDelta time to small"); 
-    break;
-  case IDDHTLIB_ERROR_NOTSTARTED: 
-    Serial.println("Error\n\r\tNot started"); 
-    break;
-  default: 
-    Serial.println("Unknown error"); 
-    break;
+    
   }
-
-   Serial.print("Temperature (oC): ");
-  Serial.println(DHT11.getCelsius(), 2);
-
-  Serial.print("Temperature (oF): ");
-  Serial.println(DHT11.getFahrenheit(), 2);
-}
-*/
-
-void toggle() 
-{
-  
-//  if (digitalRead(togglePwr) == LOW) 
-//  {
-//    modeFlag = modeFlag^1;
-//  }
-  
 }
 
