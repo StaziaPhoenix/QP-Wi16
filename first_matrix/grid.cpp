f2502da6e0a339d0eef979c8c9cb49f9412afa01
dal116 <dlee13888@gmail.com>
2016-02-29 18:14:25 -0800

    Added files via upload

--- first_matrix/grid.cpp
+++ first_matrix/grid.cpp
@@ -1,7 +1,10 @@
 #include "grid.h"
-grid::grid(int col)
+#include "vibsens.h" 
+
+grid::grid(int col), int dlay)
 {
   numCol = col; 
+  delay = dlay;
   col[numCol];
   colInit[numCol]; 
   for (int i =0; i < numCol; i++)
@@ -12,10 +15,42 @@ grid::grid(int col)
   {
     colInit[i] = 0xFF;
   }
+  pinMode(col0, OUTPUT);
+  pinMode(col1, OUTPUT); 
+  pinMode(col2, OUTPUT);
+  pinMode(col3, OUTPUT); 
+  vibsens vibe = vibsens(
 }
 void grid::defState()
 {
-  
+  for (int i = 0; i < numCol; i++)
+  {
+   for (int j = 0; j < numCol; j++)
+   {
+      if (digitalRead(toggle) == LOW) 
+      {
+        modeFlag = modeFlag^1; 
+        setData();
+        j= 0;
+        i =0;
+      }
+      digitalWrite(cols[i],HIGH);          
+      digitalWrite(cols[i],HIGH);
+      digitalWrite(latchPin,LOW);
+      shiftOut(dataPin,clockPin,MSBFIRST,colData[i]);
+      digitalWrite(latchPin,HIGH);
+      delay(100);
+      digitalWrite(cols[i], LOW); 
+      adjustData(i);
+   }
+  }
 }
+void grid::adjustData(int i) 
+{
+  if (!modeFlag) {
+     colData[i] = colData[i] << 1;
+  }
+}
+
 
 
--- first_matrix/grid.h
+++ first_matrix/grid.h
@@ -6,10 +6,15 @@ class grid
   public:
     grid(int col);
     void defState();
+    void setup(); 
+    void setData(); 
   private:
     int numCol;
+    int delay;
     char col[];
     char colInit[];
+    char col0, col1, col2, col4;
+    void adjustData(int i);
     
 };
 #endif 
