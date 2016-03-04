c11b1be029b6f6e86227a9daf93d99f1b5e2d277
Stazia Tronboll <stazia@stazias-mbp.dynamic.ucsd.edu>
2016-03-04 01:48:27 -0800

    Joystick fully functional.

--- LEDPanel/LEDPanel.ino
+++ LEDPanel/LEDPanel.ino
@@ -8,139 +8,166 @@ const int latchPin = 23;
 const int dataPin = 24;
 
 /* delay times */
-int staticDelay = 1;
-int shockDelay = 30;
-int moveDelay = 50;
+int moveDelay = 1;
 
 /* flags */
-int moveMode = 1; // 0 for static mode, 1 for move mode
+int moveMode = 1; // 0 for auto sequences, 1 for move mode
 
-/* Grid data */
+/* grid data */
 int numCol = 10;
 int numRow = 8;
 int col[10] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
 int colData[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int curs[2] = {1, 1};
 
-/* sensor pins */
-    // Joystick
+
+/* sensors */
 int joyX = A3;
 int joyY = A4;
-    // Button
-int button = 8;
-    // Vibration
-int vibrate;
-
-grid matrix = grid(numCol, moveDelay);
 controller joystick = controller(joyX, joyY, 0);
+
 
 void setup() {
   Serial.begin(9600);
-
   // Prepare shift registers
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT); 
 
-  // initialize outputs and column data
-  matrix.setData(moveMode);
   for (int i = 0; i < numCol; i++) {
     pinMode(col[i], OUTPUT);
     digitalWrite(col[i], LOW);
-    colData[i] = matrix.getData(i);
   }
 
-  // run the initialization sequence
-  for (int i = 0; i < numCol; i++) {
-    digitalWrite(col[i],HIGH);          
-    
-    digitalWrite(latchPin,LOW);
-    shiftOut(dataPin,clockPin,MSBFIRST,0xFF);
-    digitalWrite(latchPin,HIGH);
-    delay(moveDelay);
-    
-    digitalWrite(col[i], LOW);
-  }
+  colData[0] = 0x01;
 
-  delay(300);
+  initSeq();
 }
 
-/* ------------------------------
- *  Set colData to the appropriate mode - 
- *  an LED for staticMode and the run for moveMode.
- *  Then call matAct the apropriate number of times for the mode.
- */
 void loop() {
-  
-  /* call setData to prepare colData[] for the correct mode
-   * Then read the updated colData into the local version. */
   if (moveMode) {
-//    matrix.setData(moveMode);
-//    for (int i = 0; i < numCol; i++) {
-//      digitalWrite(col[i], LOW);
-//      colData[i] = matrix.getData(i);
-//    }
-    colData[0] = 0x00;
-    colData[1] = 0x02;
+    // hardcode init state for move mode
+
     moveLoop();
   }
 }
 
 void moveLoop() {
-  for (int i = 0; i < numCol; i++) { // current column
-    digitalWrite(col[i],HIGH);   
-
-    /* check if we should switch mode. If yes, return to main loop */
-//      if (digitalRead(button) == LOW)  {
-//        moveMode = moveMode^1;
-//        Serial.print("In staticLoop... ");
-//        Serial.println(moveMode);
-//        delay(300);
-//        return;
-//      }
-    /* Adapt for joystick movements */
-    checkJoystick();
-    
+  static int cntr = 0;
+  for (int i = 0; i < numCol; i++) {
     digitalWrite(latchPin,LOW);
     shiftOut(dataPin,clockPin,MSBFIRST,colData[i]);
     digitalWrite(latchPin,HIGH);
+
+    digitalWrite(col[i], HIGH);
     delay(moveDelay);
-    
     digitalWrite(col[i], LOW);
+  }
+
+  if (++cntr % 12 == 0) {
+    checkJoystick();
   }
 }
 
 void checkJoystick() {
+  static int curs[2] = {0, 0};
   int orVal;
-
   switch(curs[0]) {
     case 0: orVal = 0x01; break;
     case 1: orVal = 0x02; break;
     case 2: orVal = 0x04; break;
+    case 3: orVal = 0x08; break;
+    case 4: orVal = 0x10; break;
+    case 5: orVal = 0x20; break;
+    case 6: orVal = 0x40; break;
+    case 7: orVal = 0x80; break;
+    case 8: orVal = 0x01; break;
+    case 9: orVal = 0x02; break;
+    case 10: orVal = 0x04; break;
+    case 11: orVal = 0x08; break;
   }
+
+  int retVal;
   if (joystick.moveLeft()) {
-//    matrix.left(curs[0], curs[1]);
     if (curs[1] != 0) {
-      colData[curs[1]-1] = colData[curs[1]-1] | orVal;
-      Serial.println(colData[curs[1]-1]);
+      curs[1]--;
+      colData[curs[1]] = colData[curs[1]] | orVal;
+      Serial.println("Left");
+      Serial.println(colData[curs[1]]);
     }
-    Serial.println("Left");
   }
-//  if (joystick.moveRight()) {
-//    matrix.right();
-//    Serial.println("Right");
-//  }
-//  if (joystick.moveUp()) {
-//    matrix.up();
-//    Serial.println("Up");
-//  }
-//  if (joystick.moveDown()) {
-//    matrix.down();
-//    Serial.println("Down");
-//  }
-//  
-//  for (int i = 0; i < numCol; i++) {
-//    colData[i] = matrix.getData(i);
-//  }
+  if (joystick.moveRight()) {
+    if (curs[1] != (numCol-1)) {
+      curs[1]++;
+      colData[curs[1]] = colData[curs[1]] | orVal;
+      Serial.println("Right");
+      Serial.println(colData[curs[1]]);
+    }
+  }
+  if (joystick.moveUp()) {
+    if (curs[0] != 0) {
+      curs[0]--;
+      switch(curs[0]) {
+        case 0: orVal = 0x01; break;
+        case 1: orVal = 0x02; break;
+        case 2: orVal = 0x04; break;
+        case 3: orVal = 0x08; break;
+        case 4: orVal = 0x10; break;
+        case 5: orVal = 0x20; break;
+        case 6: orVal = 0x40; break;
+        case 7: orVal = 0x80; break;
+        case 8: orVal = 0x01; break;
+        case 9: orVal = 0x02; break;
+        case 10: orVal = 0x04; break;
+        case 11: orVal = 0x08; break;
+      }
+      colData[curs[1]] = colData[curs[1]] | orVal;
+      Serial.println("Up");
+    }
+  }
+    if (joystick.moveDown()) {
+    if (curs[0] != numRow-1) {
+      curs[0]++;
+      switch(curs[0]) {
+        case 0: orVal = 0x01; break;
+        case 1: orVal = 0x02; break;
+        case 2: orVal = 0x04; break;
+        case 3: orVal = 0x08; break;
+        case 4: orVal = 0x10; break;
+        case 5: orVal = 0x20; break;
+        case 6: orVal = 0x40; break;
+        case 7: orVal = 0x80; break;
+        case 8: orVal = 0x01; break;
+        case 9: orVal = 0x02; break;
+        case 10: orVal = 0x04; break;
+        case 11: orVal = 0x08; break;
+      }
+      colData[curs[1]] = colData[curs[1]] | orVal;
+      Serial.println("Down");
+    }
+  }
+}
+
+void initSeq() {
+  for (int i = 0; i < numCol; i++) {         
+    digitalWrite(latchPin,LOW);
+    shiftOut(dataPin,clockPin,MSBFIRST,0xFF);
+    digitalWrite(latchPin,HIGH);
+    
+    digitalWrite(col[i],HIGH);
+    delay(100);
+    digitalWrite(col[i], LOW);
+  }
+
+  for (int j = 0; j < 30; j++) {
+    for (int i = 0; i < numCol; i++) {         
+      digitalWrite(latchPin,LOW);
+      shiftOut(dataPin,clockPin,MSBFIRST,0xFF);
+      digitalWrite(latchPin,HIGH);
+      
+      digitalWrite(col[i],HIGH);
+      delay(1);
+      digitalWrite(col[i], LOW);
+    }
+  }
 }
 
